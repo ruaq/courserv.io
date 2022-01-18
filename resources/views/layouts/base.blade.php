b023895bc3114fd9a358d168f35ef46983b5f220
Holger Schmermbeck <hosch@mailbox.org>
2022-01-18 09:49:32 +0100

    added grey background

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,7 +9,7 @@
     @livewireStyles
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
-<body>
+<body class="h-full">
 
     {{ $slot }}
 
