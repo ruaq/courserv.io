b931e1b918154e4ac39d8ada36e148d4a3c8dd61
Shift <shift@laravelshift.com>
2022-02-09 16:47:08 +0000

    Shift to consistent, fluent response chains

--- routes/web.php
+++ routes/web.php
@@ -59,13 +59,13 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
-    return redirect('/home');
+    return redirect()->to('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
 
-    return back()->with('message', 'Verification link sent!');
+    return redirect()->back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::fallback(\CodeZero\LocalizedRoutes\Controller\FallbackController::class)
