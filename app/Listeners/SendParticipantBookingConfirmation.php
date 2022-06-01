77c9e05fb4e8e2a2ebc6ee535eabfbae2a7d42de
pyyjac <pyyjac@users.noreply.github.com>
2022-06-01 14:00:35 +0000

    Fix styling

--- app/Listeners/SendParticipantBookingConfirmation.php
+++ app/Listeners/SendParticipantBookingConfirmation.php
@@ -4,7 +4,6 @@ namespace App\Listeners;
 
 use App\Events\CourseBooked;
 use App\Mail\ParticipantBookingConfirmation;
-use App\Mail\PasswordReset;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
--- app/Mail/ParticipantBookingConfirmation.php
+++ app/Mail/ParticipantBookingConfirmation.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use App\Models\Course;
 use App\Models\Participant;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
